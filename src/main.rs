a3b11245bb3b7e3d8ae4290ba943a147072b8bef
joshua <blitzmasterj24@gmail.com>
2021-05-28 13:54:15 -0400

    yeah

--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,72 @@ use::std::fs::File;
 use std::io::prelude::*;
 use rand::Rng;
 
+
+const ALLOWED_ATTEMPTS: u8 = 5;
+fn main(){
+  let mut turns_left:u8 = 5;
+  let selected_word = select_word();
+  let  letters = create_letters(&selected_word);
+  
+  
+    display_progress(&letters);
+  
+
+ 
+}
+
 struct Letter {
   character: char,
   revealed: bool
-
-}
-fn main() {
-   let selected_word = selected_word();
-
-    let mut letters = create_letters(&selected_word);
 }
 
-fn selected_word() -> String{
-    //open file
+fn select_word() -> String {
+
+/*open file*/
 let mut file = File::open("words.txt").expect("could not open file");
 
-//set file contents to a string
+/*load file contents*/
 let mut file_contents = String::new();
 file.read_to_string(&mut file_contents)
-.expect("failure to write data to string");
+.expect("an error has occured");
+/*Get individual words*/
 
-//divides each word by a comma, removes white space and then collects them as a vector of strings
-let avalible_words:Vec<&str> = file_contents.trim().split(',').collect();
+let availible_words:Vec<&str> = file_contents.trim().split(',').collect();
 
-//output a random number
-let random_index = rand::thread_rng().gen_range(0,avalible_words.len());
+/*generate random index*/
+let random_index = rand::thread_rng().gen_range(0, availible_words.len());
 
-return String::from(avalible_words[random_index])
-
+ return String::from(availible_words[random_index]);
 }
 
 fn create_letters(word: &String) -> Vec<Letter>{
-    let mut letters: Vec<Letter> = Vec::new();
+  /*creates empty vector of Letter struct*/
+  let mut letters: Vec<Letter> = Vec::new();
 
-    /*takes in in string from selected word (as word)
-    then iterates through string and pushes a Letter struct
-   to the back of letters */
-    for c in word.chars(){
-        letters.push(Letter{
-           character: c,
-           revealed: false
-        });
+  /*wrap each character in a Letter struct*/
+  for c in word.chars(){
+    letters.push(Letter{
+      character: c,
+      revealed: false
+    });
+  }
+  return letters;
+}
+
+fn display_progress(letters: &Vec<Letter>){
+  let mut display_string = String::from("Progress:");
+  //Display appropriate character (letter or _) for each letter
+  for letter in letters {
+   display_string.push(' ');
+
+   if letter.revealed{
+   display_string.push(letter.character);
+    }else{
+      display_string.push('_');
     }
-    return letters
+
+    display_string.push(' ');
+
+  }
+  println!("{}",display_string);
 }
